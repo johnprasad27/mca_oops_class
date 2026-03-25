d3b27ae5c62e1027b28cc2d376168c47a30f8a5a
anandu-pn <anandupn@outlook.com>
2026-03-25 13:11:56 +0530

    new2

---
+++ Interface.java
@@ -0,0 +1,23 @@
+interface Animal {
+ void sound();  
+}
+class Dog implements Animal {
+  public void sound() {
+       System.out.println("Dog barks");
+      }
+}
+
+class Cat implements Animal {
+    public void sound() {
+    System.out.println("Cat meows");
+    }
+}
+
+public class Interface{
+        public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        Cat c = new Cat();
+        c.sound();
+    }
+}
